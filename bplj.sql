e033d9f3b6b648fec7f8c9cfb6a9b08286cb87a1
Rifqi Syekhi Marsaputra <94272149+rifqisyekhi@users.noreply.github.com>
2025-01-09 11:11:10 +0700

    Add files via upload

--- bplj.sql
+++ bplj.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 09, 2025 at 04:56 AM
+-- Generation Time: Jan 09, 2025 at 05:10 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
